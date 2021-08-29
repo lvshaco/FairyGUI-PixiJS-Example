c05eadac289e6432333dc418b5d6281c7bc26c32
lvshaco <lvshaco@gmail.com>
2021-08-29 21:40:35 +0800

    to pixi.js v5.3.8

--- up.sh
+++ up.sh
@@ -1,3 +1,3 @@
 cp ../fairygui-pixijs-v5/bin/fairygui.js  proj/dist/libs/
-cp ../h5-gui/pixi.js proj/dist/libs/
-cp ../h5-gui/pixi.js.map proj/dist/libs/
+# cp ../h5-gui/pixi.js proj/dist/libs/
+# cp ../h5-gui/pixi.js.map proj/dist/libs/
